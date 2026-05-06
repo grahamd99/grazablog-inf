3db6c6dccd5ea054ac0596877a8a81b75ab88e24
grahamd99 <graham.dodsworth@nhs.net>
2026-05-06 22:15:13 +0100

    Add CF behaviour and CF invalidation role

--- bin/app.ts
+++ bin/app.ts
@@ -9,11 +9,26 @@ const app = new cdk.App();
  */
 // Read from .env
 const tagUser = process.env.CDK_TAG_USER ?? "UNKNOWN";
+const bucketName = process.env.BUCKET_NAME;
+const stackName = process.env.STACK_NAME;
+const cfRole = process.env.CF_ROLE;
+if (!bucketName) {
+  throw new Error('BUCKET_NAME must be set in .env');
+}
+if (!stackName) {
+  throw new Error('STACK_NAME must be set in .env');
+}
+if (!cfRole) {
+  throw new Error('CF_ROLE must be set in .env');
+}
 
 // Apply tag
 cdk.Tags.of(app).add("User", tagUser);
 
 new WebsiteStack(app, "PrivateS3CloudFrontWebsiteStack", {
+  stackName,
+  bucketName,
+  cfRole,
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
